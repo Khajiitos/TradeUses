5822fa15b25fbcaabc86dfb2691fd9b96d0cd771
khajiitos <khajiit112@gmail.com>
2023-08-23 13:15:40 +0200

    Force UTF-8 for file operations
    (cherry picked from commit dfb2ba351ff6247ad5bff6a7beaa10efcc7e1ebb)

--- Common/src/main/java/me/khajiitos/tradeuses/common/config/Config.java
+++ Common/src/main/java/me/khajiitos/tradeuses/common/config/Config.java
@@ -4,6 +4,7 @@ import joptsimple.internal.Strings;
 import me.khajiitos.tradeuses.common.TradeUses;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,7 +21,7 @@ public class Config {
     public static void load() {
         if (file.exists()) {
             lines.clear();
-            try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            try (BufferedReader reader = new BufferedReader(new FileReader(file, StandardCharsets.UTF_8))) {
                 reader.lines().forEach(lines::add);
             } catch (IOException e) {
                 TradeUses.LOGGER.error("Failed to read tradeuses_text.txt", e);
@@ -32,7 +33,7 @@ public class Config {
 
     public static void save() {
         if (file.getParentFile().isDirectory() || file.getParentFile().mkdirs()) {
-            try (FileWriter fileWriter = new FileWriter(file)) {
+            try (FileWriter fileWriter = new FileWriter(file, StandardCharsets.UTF_8)) {
                 fileWriter.write(Strings.join(lines, "\n"));
             } catch (IOException e) {
                 TradeUses.LOGGER.error("Failed to save tradeuses_text.txt", e);
