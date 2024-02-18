15d322aae8260ce885c4c47dda25a2a01540a496
khajiitos <khajiit112@gmail.com>
2024-02-18 20:39:48 +0100

    Fix mixin incompatibility with ViaFabricPlus
    (cherry picked from commit 050988b88161c485d4d28669f0baf7789c385074)

---
+++ Common/src/main/java/me/khajiitos/tradeuses/common/mixin/EditBoxAccessor.java
@@ -0,0 +1,32 @@
+package me.khajiitos.tradeuses.common.mixin;
+
+import net.minecraft.client.gui.components.EditBox;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+import java.util.function.Predicate;
+
+@Mixin(EditBox.class)
+public interface EditBoxAccessor {
+    @Accessor
+    int getCursorPos();
+
+    @Accessor
+    int getHighlightPos();
+
+    @Accessor
+    int getMaxLength();
+
+    @Accessor
+    String getValue();
+
+    @Accessor
+    Predicate<String> getFilter();
+
+    @Accessor
+    void setValue(String value);
+
+    @Invoker
+    void callOnValueChange(String $$0);
+}
