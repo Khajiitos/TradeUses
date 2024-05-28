2e025a6f59ff91449c136f098350df6a34ee36d5
pt20krezolekk <103023548+pt20krezolekk@users.noreply.github.com>
2024-05-28 10:17:18 +0200

    1.20.6

--- NeoForged/src/main/java/me/khajiitos/tradeuses/neoforged/TradeUsesNeoforged.java
+++ NeoForged/src/main/java/me/khajiitos/tradeuses/neoforged/TradeUsesNeoforged.java
@@ -3,17 +3,17 @@ package me.khajiitos.tradeuses.neoforged;
 import me.khajiitos.tradeuses.common.TradeUses;
 import me.khajiitos.tradeuses.common.screen.ConfigScreen;
 import net.neoforged.api.distmarker.Dist;
-import net.neoforged.fml.DistExecutor;
 import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.neoforge.client.ConfigScreenHandler;
+import net.neoforged.fml.loading.FMLLoader;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
 @Mod(TradeUses.MOD_ID)
 public class TradeUsesNeoforged {
     public TradeUsesNeoforged() {
-        DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> {
+        if (FMLLoader.getDist() == Dist.CLIENT) {
             TradeUses.init();
-            ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () -> new ConfigScreenHandler.ConfigScreenFactory(ConfigScreen::new));
-        });
+            ModLoadingContext.get().registerExtensionPoint(IConfigScreenFactory.class, () -> ConfigScreen::new);
+        }
     }
 }
